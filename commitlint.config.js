1bfcd1d80484e2fc3593b164209fd66c0013a3ae
madavic <125142567+madavic@users.noreply.github.com>
2026-01-22 16:15:38 +0100

    refactor: update commitlint configuration to use createConfig
    - Switched to using createConfig from '@astrale/commitlint-config' for improved configuration management.
    - Defined specific scopes for commit messages: 'ui', 'components', 'styles', 'utils', 'preset', and 'constants'.

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,5 @@
-export { default } from '@astrale/commitlint-config'
+import { createConfig } from '@astrale/commitlint-config'
+
+export default createConfig({
+  scopes: ['ui', 'components', 'styles', 'utils', 'preset', 'constants'],
+})
