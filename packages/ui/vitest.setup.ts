af26c8b69ad2bab0a385e321391597a07eb273f0
madavic <125142567+madavic@users.noreply.github.com>
2026-09-21 19:18:44 +0200

    chore: migrate tests and DOM matchers to vitest 5

--- packages/ui/vitest.setup.ts
+++ packages/ui/vitest.setup.ts
@@ -1,4 +1,14 @@
-import '@testing-library/jest-dom/vitest'
+import type { TestingLibraryMatchers } from '@testing-library/jest-dom/matchers'
+
+import * as matchers from '@testing-library/jest-dom/matchers'
+import { expect } from 'vitest'
+
+// jest-dom's Vitest entrypoint still augments the pre-v5 Assertion interface.
+declare module 'vitest' {
+  interface Matchers<R, T> extends TestingLibraryMatchers<T, R> {}
+}
+
+expect.extend(matchers)
 
 class TestResizeObserver implements ResizeObserver {
   disconnect() {}
